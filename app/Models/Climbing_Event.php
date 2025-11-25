dca533f3504b491fd15fdd3a72d8a7c43893df6b
Kainy Zafari <kainy.zafari@student.ehb.be>
2025-11-25 19:51:24 +0100

    add migrations and models for climbing events and their relationships

---
+++ app/Models/Climbing_Event.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Climbing_Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'location', 'date', 'description'];
+
+    protected $casts = [
+        'date' => 'datetime',
+    ];
+
+    // Relationship: An Event has many Users
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
